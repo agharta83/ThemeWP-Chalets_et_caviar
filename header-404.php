1ae501786a8b734f8263259dc1053462f01f67bd
agharta83 <35310694+agharta83@users.noreply.github.com>
2020-01-27 22:29:54 +0100

    add page 404

---
+++ header-404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset=<?php bloginfo('charset'); ?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+  <div class="wrapper">   
+
+  <?php get_template_part( 'template-parts/content/404', 'hero' ); ?>
+
+    <header class="header">
+      <div class="header__logo">
+        <a href="<?php echo home_url('/'); ?>">Chalets et caviar</a>
+      <div class="header__logo-baseline">Agence immobilière à Courchevel</div>
+      </div>
+
+      <nav class="main-nav">
+        <a href="<?php echo home_url('/'); ?>" class="main-nav__link">Accueil</a>
+        <a href="<?php echo get_term_link($term = 'vente', $taxonomy = 'type_annonce'); ?>" class="main-nav__link">Ventes</a>
+        <a href="<?php echo get_term_link($term = 'location', $taxonomy = 'type_annonce'); ?>" class="main-nav__link">Locations</a>
+        <a href="#contact" class="main-nav__link">Contact</a>
+      </nav>
+    </header>
---
+++ template-parts/content/404-hero.php
@@ -0,0 +1,8 @@
+<section class="hero-taxo">
+      <div class="hero__bcg-opacity">
+        <div class="hero__title-wrapper">
+          <h1 class="hero__title">Erreur 404</h1>
+          <h3 class="hero__description">Page non trouvée</h3>
+        </div>      
+      </div>
+    </section>
